af5b5fc87780bd9a48fc3cd39c6ee283c35f7594
Bruce Nielson <brucenielson@users.noreply.github.com>
2020-05-21 23:53:26 -0600

    Improved version using ConsCase

--- IntList.hs
+++ IntList.hs
@@ -4,9 +4,6 @@
 data ConsCase = ConsType Int IntList deriving (Show)
 data IntList =  Nil | Cons ConsCase deriving (Show)
 
-mkConsCase :: Int -> IntList -> ConsCase
-mkConsCase x lst = ConsType x lst
-
 mkIntList :: [Int] -> IntList
 mkIntList [] = Nil
 mkIntList (x:xs) = Cons (ConsType x (mkIntList xs))
@@ -14,14 +11,7 @@ mkIntList (x:xs) = Cons (ConsType x (mkIntList xs))
 
 list = mkIntList [1, 2, 3]
 
-consCaseHead :: ConsCase -> Int
-consCaseHead (ConsType x _) = x
-
-consCaseTail :: ConsCase -> IntList
-consCaseTail (ConsType _ xs) = xs
-
-
-
+-- Direct IntList Functions - Corresponds to Jerry Swan's Listing 2
 lenIntList :: IntList -> Int
 lenIntList Nil = 0
 lenIntList xs = 1 + lenIntList xs
@@ -34,9 +24,22 @@ sumIntList :: IntList -> Int
 sumIntList Nil = 0
 sumIntList (Cons cons) = consCaseHead cons + sumIntList (consCaseTail cons) 
 
-foldIntList :: (ConsCase -> Int -> Int) -> Int -> IntList -> Int
-foldIntList _ acc Nil = acc
-foldIntList f acc (Cons xs) = f (ConsType (consCaseHead xs) Nil) (foldIntList f acc (consCaseTail xs)) 
+
+
+-- New code required so that I can split a ConsCase into a head and tail.
+-- I used to be able to just do this via pattern matching. Now that doesn't work because
+-- "ConsCase" isn't an "IntType" and vice versa. 
+consCaseHead :: ConsCase -> Int
+consCaseHead (ConsType x _) = x
+
+consCaseTail :: ConsCase -> IntList
+consCaseTail (ConsType _ xs) = xs
+
+
+-- Fold Functions - Corresponds to Jerry Swan's Listing 3
+foldList :: IntList -> Int -> (ConsCase -> Int -> Int) -> Int
+foldList Nil acc _ = acc
+foldList (Cons xs) acc f = f (ConsType (consCaseHead xs) Nil) (foldList (consCaseTail xs) acc f)
 
 lenConsCase :: ConsCase -> Int -> Int
 lenConsCase _ acc = 1 + acc
@@ -44,21 +47,22 @@ lenConsCase _ acc = 1 + acc
 sumConsCase :: ConsCase -> Int -> Int
 sumConsCase cons acc = consCaseHead cons + acc
 
-foldList :: IntList -> Int -> (ConsCase -> Int -> Int) -> Int
-foldList lst acc f = foldIntList f acc lst
 
+-- Examples of how to use
 -- foldList list 0 sumConsCase
 -- returns 6
 -- foldList list 0 lenConsCase
 -- returns 3
 
 
+-- Final Functions using folds
 length' :: IntList -> Int
 length' lst = foldList lst 0 lenConsCase
 
 sum' :: IntList -> Int
 sum' lst = foldList lst 0 sumConsCase
 
+-- Examples of how to use
 -- length' list
 -- returns 3
 -- sum' list
